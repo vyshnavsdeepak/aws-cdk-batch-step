dfdc6f19a9897e5acef7e267de9d987ae63abf4d
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2024-12-03 14:07:05 +0530

    Initial commit

---
+++ test/doc-ai-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DocAiPipeline from '../lib/doc-ai-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/doc-ai-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DocAiPipeline.DocAiPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
